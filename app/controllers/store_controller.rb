f0cdd2de95faaa8a4fb635308076b557c42b74b0
Madh93 <michelangelo93@gmail.com>
2017-09-11 00:35:55 +0100

    Record how many times the user has accessed the store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,14 @@
 class StoreController < ApplicationController
+  before_action :initialize_counter
+
   def index
+    session[:counter] += 1
     @products = Product.order(:title)
+  end
+
+  private
+
+  def initialize_counter
+    session[:counter] ||= 0
   end
 end
